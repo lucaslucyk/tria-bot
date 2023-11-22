3bed5d41646db93115438232cc04fcf73d536e88
Lucas Lucyk <llucyk@grupospec.com>
2023-11-22 13:09:56 -0300

    message queue with redis pubsub

--- tria_bot/crud/base.py
+++ tria_bot/crud/base.py
@@ -1,6 +1,6 @@
 from redis.asyncio.client import Redis
 from abc import ABC, abstractproperty
-from typing import Generic, Optional, Type, TypeVar
+from typing import Any, Generic, Iterable, Optional, Type, TypeVar
 from tria_bot.models.base import HashModelBase
 from pydantic import BaseModel
 
@@ -9,13 +9,31 @@ CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 
+class Decorators:
+    @staticmethod
+    def use_connection(name: str):
+        def decorator(func):
+            async def wrapper(crud: CRUDBase, *args, **kwargs):
+                conn = kwargs.pop(name, None)
+                if conn != None:
+                    crud.model._meta.database = kwargs[name]
+                return await func(crud, *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+
 class CRUDBase(Generic[ModelType], ABC):
     @abstractproperty
     def model(self) -> Type[ModelType]:
         ...
 
+    @Decorators.use_connection("conn")
     async def get(
-        self, pk: str, conn: Optional[Redis] = None
+        self,
+        pk: Any,
+        conn: Optional[Redis] = None,
     ) -> Optional[ModelType]:
         """Get row from model by uid
 
@@ -26,7 +44,14 @@ class CRUDBase(Generic[ModelType], ABC):
         Returns:
             Optional[ModelType]: ModelType instance or None if id not exists
         """
-        if conn != None:
-            self.model._meta.database = conn
-
+        # if conn != None:
+        #     self.model._meta.database = conn
         return await self.model.get(pk=pk)
+
+    @Decorators.use_connection("conn")
+    async def get_all(
+        self,
+        conn: Optional[Redis] = None,
+    ):
+        async for pk in self.model.all_pks():
+            yield await self.get(pk=pk)
