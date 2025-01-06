77fd3aff9e2320c14a152a2210902be2ed1f2af4
Lucas Lucyk <lucaslucyk@gmail.com>
2025-01-05 22:05:51 -0300

    retry get symbols at depth start

--- pkg/stream/depths/symbols.go
+++ pkg/stream/depths/symbols.go
@@ -1,11 +1,24 @@
 package depths
 
 import (
+	"time"
+
+	"github.com/lucaslucyk/tria-bot/shared/logger"
 	dbm "github.com/lucaslucyk/tria-bot/shared/models/db"
 	"github.com/lucaslucyk/tria-bot/shared/redis"
 )
 
 func GetSymbols() ([]string, error) {
+	logger.Info("Getting symbols from db...")
 	sr := redis.NewRepository[dbm.Symbol]()
-	return sr.Ids(true)
+	s, err := sr.Ids(true)
+	if err != nil {
+		return s, err
+	}
+	if len(s) == 0 {
+		logger.Info("No symbols yet, trying again in 3 seconds...")
+		time.Sleep(3 * time.Second)
+		return GetSymbols()
+	}
+	return s, nil
 }
